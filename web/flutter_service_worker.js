75ac8d87debac88adff1d52932a96ac781e58489
joaovictoroliveirapereira6522@gmail.com <joaovictoroliveirapereira6522@gmail.com>
2020-02-27 18:18:47 -0300

    Nova versão do app WEB com um alert para demostrar que está no web

--- web/flutter_service_worker.js
+++ web/flutter_service_worker.js
@@ -13,7 +13,7 @@ const RESOURCES = {
 "/icons\Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "/icons\Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "/index.html": "b7d4a479cd81aac4a5d3f29929e32f27",
-"/main.dart.js": "60baa859c08567a0c594a46ca01ecd6b",
+"/main.dart.js": "b369e4b502ad334bbee08df95c822f19",
 "/manifest.json": "596d12433411793da1253a524985cee5"
 };
 
